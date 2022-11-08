e00e45a5bd81dc55865723335c01744571cac0da
Hana Acciaioli <hana.acciaioli@gmail.com>
2022-11-07 18:35:43 -0800

    C:/Program Files/Git/books route test written and failing

---
+++ __tests__/books.test.js
@@ -0,0 +1,20 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('books routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  it('/books route should return a list of books', async () => {
+    const res = await request(app).get('/books');
+    expect(res.body.length).toEqual(10);
+    const beloved = res.body.find((char) => char.id === '1');
+    expect(beloved).toHaveProperty('title', 'Beloved');
+    expect(beloved).toHaveProperty('released', '1987');
+  });
+  afterAll(() => {
+    pool.end();
+  });
+});
